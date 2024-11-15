b093791c39e91de2682eaa3cef90bc11fa6cdcd2
mr.Robot <zanid227@yandex.ru>
2024-11-15 20:06:22 +0300

    work in progress

--- models/db_models.py
+++ models/db_models.py
@@ -8,6 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+class RoleUserEnum(str, enum.Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+
+
 class TypeQuestionEnum(str, enum.Enum):
     ANSWER_TEXT = 'Ответ текстом'
     ANSWER_CHOICE_ONE = 'Ответ выбором 1 варианта'
@@ -18,12 +23,29 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True, index=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    username: Mapped[str] = mapped_column(
+        unique=True, index=True
+    )
+    email: Mapped[str] = mapped_column(
+        unique=True, index=True
+    )
+    password: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    role: Mapped[RoleUserEnum] = mapped_column(
+        Enum(RoleUserEnum),
+        default=RoleUserEnum.USER,
+        create_type=False
+    )
+    polls = relationship(
+        'Poll', back_populates='admin', cascade='all, delete, delete-orphan'
+    )
 
-    polls = relationship('Poll', back_populates='admin', cascade='all, delete, delete-orphan')
+    # Связи
+    user_results = relationship('UserResult', back_populates='user')
 
 
 
@@ -31,23 +53,65 @@ class Poll(Base):
 
     __tablename__ = 'polls'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
-    date_start: Mapped[date] = mapped_column(nullable=False)
-    date_end: Mapped[date] = mapped_column(nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    title: Mapped[str] = mapped_column(
+        unique=True, index=True, nullable=False
+    )
+    date_start: Mapped[date] = mapped_column(
+        nullable=False
+    )
+    date_end: Mapped[date] = mapped_column(
+        nullable=False
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), nullable=False
+    )
 
+    # Связи
     user = relationship('User', back_populates='polls')
     questions = relationship('Question', back_populates='poll', cascade='all, delete, delete-orphan')
+    user_results = relationship('UserResult', back_populates='poll')
 
 
 class Question(Base):
 
     __tablename__ = 'questions'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
-    answer: Mapped[TypeQuestionEnum] = mapped_column(Enum(TypeQuestionEnum), default=TypeQuestionEnum.ANSWER_CHOICE_ONE)
-    poll_id: Mapped[int] = mapped_column(ForeignKey('polls.id'), nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    title: Mapped[str] = mapped_column(
+        unique=True, nullable=False, index=True
+    )
+    answer: Mapped[TypeQuestionEnum] = mapped_column(
+        Enum(TypeQuestionEnum),
+        default=TypeQuestionEnum.ANSWER_CHOICE_ONE,
+        create_type=False
+    )
+    poll_id: Mapped[int] = mapped_column(
+        ForeignKey('polls.id'), nullable=False
+    )
 
+    # Связи
     poll = relationship('Poll', back_populates='questions')
+
+
+class UserResult(Base):
+
+    __tablename__ = 'user_results'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), nullable=False
+    )
+    poll_id: Mapped[int] = mapped_column(
+        ForeignKey('polls.id'), nullable=False
+    )
+
+    # Связи
+    user = relationship('User', back_populates='user_results')
+    poll = relationship('Poll', back_populates='user_results')
